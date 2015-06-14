c6ae174063ec61d3e3a3638d72d6c3505a0579a4
muszu <marcin.muszalski@student.uj.edu.pl>
2015-06-14 23:37:27 +0200

    up

--- function/wyniki skoki seria.sql
+++ function/wyniki skoki seria.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION wyniki_skoki_seria(INT) AS $$
+CREATE FUNCTION wyniki_skoki_seria(INT) RETURNS TABLE (id_zawodnika INT, punkty NUMERIC(5,1)) AS $$
 SELECT id_zawodnika,
     CASE WHEN status='OK' THEN odleglosc+sedzia1+sedzia2+sedzia3+sedzia4+sedzia5 -
                                 GREATEST(sedzia1, sedzia2, sedzia3, sedzia4, sedzia5) +
