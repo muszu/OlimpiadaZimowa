853c2db540c457983aa49d5de388d015ac6ca830
KubaSz4 <szalus@onet.eu>
2015-06-14 22:22:41 +0200

    wyniki_skoki(id_dyscypliny) + druzynowe

--- function/wyniki_skoki.sql
+++ function/wyniki_skoki.sql
@@ -1,5 +1,12 @@
-CREATE OR REPLACE FUNCTION wyniki_skoki(INT, INT) RETURNS TABLE (id_zawodnika INT, punkty NUMERIC(5,1)) AS $$
-SELECT A.id_zawodnika, COALESCE(A.punkty, 0) + COALESCE(B.punkty, 0)
-        FROM wyniki_skoki_seria($1) A LEFT OUTER JOIN wyniki_skoki_seria($2) B;
+CREATE OR REPLACE FUNCTION wyniki_skoki(INT) RETURNS TABLE (id_zawodnika INT, punkty NUMERIC(5,1)) AS $$
+DECLARE
+	Iseria INT;
+	IIseria INT;
+BEGIN
+	SELECT INTO Iseria id_rozgrywki FROM rozgrywki JOIN fazy USING(id_fazy) WHERE id_dyscypliny = $1 AND fazy.nazwa = "I seria";
+	SELECT INTO IIseria id_rozgrywki FROM rozgrywki JOIN fazy USING(id_fazy) WHERE id_dyscypliny = $1 AND fazy.nazwa = "II seria";
+	RETURN QUERY (SELECT A.id_zawodnika, COALESCE(A.punkty, 0) + COALESCE(B.punkty, 0)
+		FROM wyniki_skoki_seria(Iseria) A LEFT OUTER JOIN wyniki_skoki_seria(IIseria) B USING(id_zawodnika) ORDER BY 2 DESC);
+END;
 $$
-LANGUAGE SQL;
+LANGUAGE plpgSQL;
--- function/wyniki_skoki_druzynowe.sql
+++ function/wyniki_skoki_druzynowe.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION wyniki_skoki_druzynowe(INT, INT) RETURNS TABLE (id_druzyny INT, punkty NUMERIC(5,1)) AS $$
+CREATE OR REPLACE FUNCTION wyniki_skoki_druzynowe(INT) RETURNS TABLE (id_druzyny INT, punkty NUMERIC(5,1)) AS $$
 SELECT id_druzyny, SUM(punkty)
-        FROM (SELECT id_druzyny FROM druzyny WHERE id_dyscypliny = ##skoki narciarskie druzynowe##) A
+        FROM (SELECT id_druzyny FROM druzyny WHERE id_dyscypliny = $1) A
             JOIN zawodnicy_druzyny USING (id_druzyny) B
-            JOIN wyniki_skoki($1, $2) USING (id_zawodnika) C
+            JOIN wyniki_skoki($1) USING (id_zawodnika) C
         GROUP BY (id_druzyny)
         ORDER BY 2 desc
 $$
