43f74967ea44c06388efb6456ac02a4e196b0ae1
muszu <marcin.muszalski@student.uj.edu.pl>
2015-06-14 23:16:52 +0200

    it should work (medale druzynowe w skokach)

--- function/skoki_dru_wyniki_f.sql
+++ function/skoki_dru_wyniki_f.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION skoki_ind_wyniki_f(x integer) 
+CREATE OR REPLACE FUNCTION skoki_dru_wyniki_f(x integer) 
 RETURNS TABLE(	dyscyplina integer, id_zawodnika integer, id_druzyny integer, medal integer, suma numeric(4,1)) AS $$
 DECLARE
 	suma1 numeric(4,1);
@@ -16,16 +16,20 @@ BEGIN
 	suma3=-42;
 	iterator = 1;
 	tempsuma=-43;
-	FOR r IN SELECT  DISTINCT ws.punkty FROM wyniki_skoki_druzynowe(x) ws ORDER BY 1 DISC;
+	FOR r IN SELECT  DISTINCT ws.punkty FROM wyniki_skoki_druzynowe(x) ws ORDER BY 1 DESC
 	LOOP
 		IF (iterator = 1) THEN suma1 = ws.punkty; iterator=iterator+1; END IF;
-		IF (iterator = 2) THEN suma2 = ws.punkty; iterator=iterator+1 END IF;
-		IF (iterator = 3) THEN suma3 = ws.punkty; END IF;
+		IF (iterator = 2) THEN suma2 = ws.punkty; iterator=iterator+1; END IF;
+		IF (iterator = 3) THEN suma3 = ws.punkty; iterator=iterator+1; END IF;
 	END LOOP;
 	IF (suma1 = suma2) THEN suma2 = -42; END IF;
 	IF( suma1 = suma3) THEN suma3 = -42; END IF;
 	if( suma2 = suma3) THEN suma3 = -42; END IF;
 	RETURN QUERY 
-	SELECT x, s1.id_zawodnika, ;
+	SELECT x, wsd.id_druzyny, CASE WHEN wsd.punkty = suma1 THEN 1
+					 WHEN wsd.punkty = suma2 THEN 2
+					 WHEN wsd.punkty = suma3 THEN 3
+				END
+	FROM wyniki_skoki_druzynowe(x) wsd WHERE wsd.punkty;
 END;
 $$ LANGUAGE plpgsql;
