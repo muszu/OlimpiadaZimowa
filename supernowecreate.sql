6a6bfc62d49c0f9403f654f1908bc3c37beb5980
muszu <marcin.muszalski@student.uj.edu.pl>
2015-06-15 02:50:50 +0200

    works

--- supernowecreate.sql
+++ supernowecreate.sql
@@ -1967,10 +1967,22 @@ END IF;
 END;
 $zabron$ LANGUAGE plpgsql;
 
+
+
+CREATE OR REPLACE FUNCTION zabron_skoki() RETURNS trigger AS $zabron_skoki$
+BEGIN
+ 
+ IF((SELECT k.nazwa FROM kategorie k JOIN dyscypliny d ON d.id_kategorii = k.id JOIN rozgrywki r ON d.id = r.id_dyscypliny
+ WHERE r.id_rozgrywki = NEW.id_rozgrywki) != 'skoki narciarskie')
+	THEN RAISE EXCEPTION 'Rozgrywka nie jest z tej dyscypliny';
+ ELSE RETURN NEW;
+ END IF;
+
+END;
+$zabron_skoki$ LANGUAGE plpgsql;
+
 CREATE TRIGGER zabron_skoki BEFORE INSERT OR UPDATE ON skoki_narciarskie
 FOR EACH ROW EXECUTE PROCEDURE zabron_skoki();
-
-
 
 CREATE OR REPLACE FUNCTION zabron_sedz() RETURNS trigger AS $zabron_sedz$
 DECLARE
@@ -2008,17 +2020,7 @@ FOR EACH ROW EXECUTE PROCEDURE zabron_biegi();
 
 ------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION zabron_skoki() RETURNS trigger AS $zabron_skoki$
-BEGIN
- 
- IF((SELECT k.nazwa FROM kategorie k JOIN dyscypliny d ON d.id_kategorii = k.id JOIN rozgrywki r ON d.id = r.id_dyscypliny
- WHERE r.id_rozgrywki = NEW.id_rozgrywki) != 'skoki narciarskie')
-	THEN RAISE EXCEPTION 'Rozgrywka nie jest z tej dyscypliny';
- ELSE RETURN NEW;
- END IF;
 
-END;
-$zabron_skoki$ LANGUAGE plpgsql;
 
 
 
