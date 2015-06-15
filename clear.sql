cf38c8596bbdc8c3f40c2add2abad1999d6f4fe5
muszu <marcin.muszalski@student.uj.edu.pl>
2015-06-15 02:39:50 +0200

    CLEAR OSOM CLEAR

--- clear.sql
+++ clear.sql
@@ -11,3 +11,19 @@ DECLARE r record;
         END LOOP;
     END
 	$$;
+	
+DO $$
+DECLARE r record;
+    BEGIN
+        FOR r IN SELECT ns.nspname, proname, proargtypes FROM pg_proc INNER JOIN pg_namespace ns ON (pg_proc.pronamespace = ns.oid)
+ where nspname = 'public' AND proname IN('refresh_medale_ind','refresh_test' ,'doping_change' ,'test' ,'test' ,'refresh_medale_ind' ,'wyniki_skoki' ,'wyniki_skoki_seria' ,
+'refresh_medale' ,'sprawdz1' ,'zabron_sedz' ,'zabron_lyzw' ,'sprawdz2' ,'sprawdz_dyscypline' ,'sprawdz_plec' ,
+'zabron_hokej' ,'sedziowie_delete_check' ,'sprawdz_plec_kraj' ,'zabron_biegi' ,'zabron_skoki' ,'odswiez_medale' ,
+'wyniki_dru_bezsk' ,'wyniki_ind' ,'wyniki_skoki_druzynowe' ,'zabron' ,'skoki_dru_wyniki_f' ,'skoki_ind_wyniki_f' ,
+'wyniki_ind_bezwyjatku' ,'skoki_update' ,'dru_wyniki_bezsk_f' ,'tabela_medalowa_nadkategoria' ,
+'tabela_medalowa_nadkategoria_ind' ,'tabela_medalowa_nadkategoria_zes')
+        LOOP
+			EXECUTE 'DROP FUNCTION ' || r.nspname || '.' || r.proname || '(' || oidvectortypes(r.proargtypes) || ') CASCADE;';
+        END LOOP;
+    END
+	$$;	
